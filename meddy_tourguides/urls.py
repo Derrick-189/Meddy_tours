37f1a3fd9d0bb637375c6505a9d90405858306c4
Slade <sladephantom60@gmail.com>
2025-09-15 19:16:37 +0300

    first commit

---
+++ meddy_tourguides/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('destination.html', views.destination, name='destination'),
+    path('discount.html', views.discount, name='discount'),
+    path('about.html', views.about, name='about'),
+    path('blog.html', views.blog, name='blog'),
+    path('contact.html', views.contact, name='contact'),
+    path('booking.html', views.booking, name='booking'),
+]
